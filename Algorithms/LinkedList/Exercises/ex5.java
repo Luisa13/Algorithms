3b8746bc27266cd2c1ac332228c6456e1c5e1c1d
Luisa <luisatrece@gmail.com>
2021-01-07 12:57:45 +0100

    Improvement ex5

--- Algorithms/LinkedList/Exercises/ex5.java
+++ Algorithms/LinkedList/Exercises/ex5.java
@@ -1,8 +1,27 @@
 package Exercises;
 
+/**
+ * Given two numbers represented by a linked list, where each node contains a single digit. 
+ * The digits are stored in reverse order, such the 1st digit is at the head of the list. The
+ * class provide some methods to returns the sum as a linked list.
+ * 
+ * Example:
+ * 		617 + 295 : 7->1->6  +  5->9->2
+ * 		912	:	2->1->9
+ *  
+ *  @author luisa
+ * */
 public class ex5 {
 
-	public LinkedList<Integer> sumList( LinkedList<Integer>l1, LinkedList<Integer> l2) {
+	/**
+	 * Returns a linked list that represent the sum of two linked lists of integers 
+	 * whose digits are stored in reverse order.
+	 * 
+	 * @param l1 First term represented as a singly linked list of integers
+	 * @param l2 Second term represented as a singly linked list of integers
+	 * @return result
+	 * */
+	public LinkedList<Integer> sumListReverse( LinkedList<Integer>l1, LinkedList<Integer> l2) {
 		int dec = 1;
 		int sum = 0;
 		
@@ -25,4 +44,30 @@ public class ex5 {
 		
 		return result;
 	}
+	
+	/**
+	 * FOLLOW UP
+	 * 
+	 * Returns a linked list that represent the sum of another two linked lists
+	 * of integers whose digits are stored in forward order
+	 * */
+	public LinkedList<Integer> sumList( LinkedList<Integer>l1, LinkedList<Integer> l2) {
+		int digit = 0;
+		LinkedList<Integer> result = new LinkedList<Integer>();
+		
+		while (l1.head != null && l2.head != null) {
+			double val = (l1.current() + l2.current() + digit)* 0.1;
+			digit = (int)val;
+			int decimal = (int)((val - digit) *10);
+			result.insert(decimal);
+			
+			l1.head = l1.next();
+			l2.head = l2.next();
+			
+		}
+		
+		return result;
+	}
+	
+	
 }
