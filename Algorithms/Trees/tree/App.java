0dc13a3686ee9d1916f24ba035b62986bed342f3
Luisa <luisatrece@gmail.com>
2021-01-28 23:08:25 +0100

    Add basic way of traverse the binary tree

---
+++ Algorithms/Trees/tree/App.java
@@ -0,0 +1,13 @@
+package tree;
+
+public class App {
+
+	public static void main(String[] args) {
+		BinaryTree<Integer> bt = new BinaryTree<Integer>(1);
+		bt.appendLeft(8);
+		bt.appendRight(2);
+
+
+	}
+
+}
--- Algorithms/Trees/tree/BinaryTree.java
+++ Algorithms/Trees/tree/BinaryTree.java
@@ -1,12 +1,16 @@
 package tree;
 
+import java.sql.PreparedStatement;
+
 /**
+ * Represents a binary tree
  * 
+ * @author luisa
  * */
 public class BinaryTree<T> {
 	
 	/**
-	 * 
+	 * Node for a binary tree
 	 * */
 	public static class Node<T>{
 		T value;
@@ -76,6 +80,71 @@ public class BinaryTree<T> {
 	 * */
 	public Node<T> right(){
 		return this.root.right;
+	}
+	
+	/**
+	 * Prints the node of the tree in InOrder
+	 * 
+	 * @param Node Current node to start to print
+	 * */
+	private void inOrder(Node node) {
+		if(node == null)
+			return;
+		System.out.print(node.value + " ");
+		inOrder(node.left);
+		inOrder(node.right);
+	}
+	
+	/**
+	 * Prints the node of the tree in InOrder from the root
+	 * */
+	public void inOrder() {
+		inOrder(this.root);
+		System.out.println();
+	}
+	
+	/**
+	 * Prints the node of the tree in PreOrder
+	 * 
+	 * @param Node Current node to start to print
+	 * */
+	private void preOrder(Node node) {
+		if(node == null)
+			return;
+		
+		preOrder(node.left);
+		System.out.print(node.value);
+		preOrder(node.right);
+	}
+	
+	/**
+	 * Prints the node of the tree in PreOrder from the root
+	 * */
+	public void preOrder() {
+		preOrder(this.root);
+		System.out.println();
+	}
+	
+	/**
+	 * Prints the node of the tree in PostOrder
+	 * 
+	 * @param Node Current node to start to print
+	 * */
+	private void postOrder(Node  node) {
+		if(node == null)
+			return;
+		
+		postOrder(node.left);
+		postOrder(node.right);
+		System.out.print(node.value);
+	}
+	
+	/**
+	 * Prints the node of the tree in PostOrder from the root
+	 * */
+	public void postOrder() {
+		postOrder(this.root);
+		System.out.println();
 	}
 
 }
