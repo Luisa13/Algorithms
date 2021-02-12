1c12c688f5e62ccc5d823254a888281f27a7f45d
Luisa <luisatrece@gmail.com>
2021-02-12 09:31:46 +0100

    init exercise 2 for trees

--- Algorithms/Trees/trees/exercises/ex2.java
+++ Algorithms/Trees/trees/exercises/ex2.java
@@ -1,5 +1,28 @@
 package trees.exercises;
 
+import tree.BinaryTree;
+
+/**
+ * Given a sorted (increased order) array with unique integer elements, 
+ * write an algorithm to create a binary search tree with minimal height.
+ * 
+ * @author luisa
+ * */
 public class ex2 {
+	
+	public static class BST extends BinaryTree<Integer>{
+		private Node<Integer> root;
+		
+		public BST(){
+			this.root = null;
+		}
+		
+		/*private void insert(int value, Node<Integer> node){
+			if()
+		}*/
+		
+		
+	}
+		
 
 }
