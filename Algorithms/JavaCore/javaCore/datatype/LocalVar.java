94d32763a3a61361da9d784ccd91239323627bc5
Luisa <luisatrece@gmail.com>
2021-02-14 14:21:19 +0100

    examples local variable and type interference

---
+++ Algorithms/JavaCore/javaCore/datatype/LocalVar.java
@@ -0,0 +1,24 @@
+package javaCore.datatype;
+
+public  class LocalVar {
+	
+	// Global var variable is not allowed
+	//var variable = "Hello";
+	
+	public static void typeOf() {
+		var s = "Hey there! ";
+		// Is not allowed
+		// var variable = null;
+		System.out.print(s instanceof String);
+	}
+	
+	public static void whenVarInitWithAnonymous_thenGetAnonymousType()	{
+		var obj = new Object() {};
+		System.out.print(obj.getClass().equals(Object.class));
+		
+		 // obj = new Object();  //Error Type mismatch
+	}
+	
+	
+	
+}
