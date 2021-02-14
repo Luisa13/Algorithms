e3a3f299d08de38d4b2e90642ea20458b09cc519
Luisa <luisatrece@gmail.com>
2021-02-14 14:20:32 +0100

    data type examples

--- Algorithms/JavaCore/javaCore/datatype/App.java
+++ Algorithms/JavaCore/javaCore/datatype/App.java
@@ -3,18 +3,10 @@ package javaCore.datatype;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String str = "Hello";
-		String str2 = str;
-		str2 = "I've changed! ";
-		System.out.println("str: " + str + "\nstr2: " + str2);
+		 LocalVar.typeOf();
+		 LocalVar.whenVarInitWithAnonymous_thenGetAnonymousType();
 		
-		StringBuilder sb = new StringBuilder("Hello");
-		StringBuilder sb2 = sb;
-		sb2.append(" changing!");
-		System.out.println("str: " + sb + "\nstr2: " + sb2);
-		//System.out.println(str.hashCode() + " and " + c.hashCode());
-		
+
 		
 
 	}
