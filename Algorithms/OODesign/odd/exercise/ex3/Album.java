e581d9cff85b881afabec2bde34ba934ee61a2d2
Luisa <luisatrece@gmail.com>
2021-03-09 14:51:10 +0100

    Album for exercise 3

--- Algorithms/OODesign/odd/exercise/ex3/Album.java
+++ Algorithms/OODesign/odd/exercise/ex3/Album.java
@@ -8,22 +8,24 @@ public class Album {
 	private String artist;
 	private List<Song> songs;
 	
-	public Album() {
-		
+	public Album(String title, String artist, List<Song> songs) {
+		this.title = title;
+		this.artist = artist;
+		this.songs = songs;
 	}
 	public String getTitle() {
-		
+		return this.title;
 	}
 	
 	public String getArtist() {
-		
+		return this.artist;
 	}
 	
 	public List<Song> getSongs(){
-		
+		return this.songs;
 	}
-	public void addSong() {
-
+	public void addSong(Song newSong) {
+		this.songs.add(newSong);
 	}
 
 }
