cbf001896f29013c611815435fd0f387593a9da4
Luisa <luisatrece@gmail.com>
2021-03-10 10:51:10 +0100

    complete round of blackjack

--- Algorithms/OODesign/ood/app/BlackJackGame.java
+++ Algorithms/OODesign/ood/app/BlackJackGame.java
@@ -1,5 +1,7 @@
 package ood.app;
 
+import java.util.Scanner;
+
 import ood.exercises.ex1.BlackJackCard;
 import ood.exercises.ex1.BlackJackHand;
 import ood.exercises.ex1.Deck;
@@ -12,15 +14,32 @@ public  class BlackJackGame {
 		bjCards.shuffle();
 		BlackJackHand hand = new BlackJackHand(bjCards.deliverHand(2));
 		BlackJackHand hand2 = new BlackJackHand(bjCards.deliverHand(2));
+		Scanner sc = new Scanner(System.in);
 		
 		//==========================================
+
+		System.out.println("LET'S PLAY");
+		System.out.println("Your score is: " + hand.score() + 
+				"\nAnother card? (y/n)");
+		String response = sc.next();
+		
+		while(response.equals("y")) {
+			hand.add(bjCards.handOverACard());
+			System.out.println("Your score is: " + hand.score() + 
+					"\nAnother card? (y/n): ");
+			response = sc.next();
+		}
+		
+		
+		
+		
 		System.out.println("Player: " + hand.score());
 		System.out.println("Cruppier: " + hand2.score());
 		if(hand.isTheWinner(false, hand2)) {
-			System.out.print("you have won");
+			System.out.print("Congrats! You have won!!");
 		}
 		else {
-			System.out.print("Cruppier won");
+			System.out.print("Cruppier won...");
 		}
 	}
 
