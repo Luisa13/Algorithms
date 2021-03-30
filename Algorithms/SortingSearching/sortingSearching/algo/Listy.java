3db967acd2c33ebce30d3ddd24602441c95abc80
Luisa <luisatrece@gmail.com>
2021-03-30 09:40:06 +0200

    listy strcuture for exercise 4

---
+++ Algorithms/SortingSearching/sortingSearching/algo/Listy.java
@@ -0,0 +1,17 @@
+package sortingSearching.algo;
+
+import java.util.ArrayList;
+
+public class Listy extends ArrayList<Integer> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+	public Integer elementAt(int index) {
+		return this.get(index); 
+	} 
+
+}
