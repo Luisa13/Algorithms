32dc0928af26cdd1568de930d03d7fe433db733c
Luisa <luisatrece@gmail.com>
2021-04-11 08:51:32 +0200

    OODesign: Page class in exercise 5

--- Algorithms/OODesign/ood/exercises/ex5/Page.java
+++ Algorithms/OODesign/ood/exercises/ex5/Page.java
@@ -1,5 +1,40 @@
 package ood.exercises.ex5;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+/**
+ * Represents a page of the book
+ * @author luisa
+ * */
 public class Page {
 
+	private int pageNum;
+	private String strPage;
+	private File pageFile;
+	
+	/**
+	 * 
+	 * */
+	public Page(String filename) {
+		this.strPage = "";
+		this.pageFile = new File(filename);
+		try {
+			this.strPage = new String (Files.readAllBytes(Paths.get(filename)));
+			this.pageNum ++;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * 
+	 * */
+	public void display() {
+		System.out.print(strPage);
+	}
 }
+		
+	
