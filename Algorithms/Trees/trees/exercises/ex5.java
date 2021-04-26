fcba5da94295c1e29c93778ace4a6f8e0ba711c0
Luisa <luisatrece@gmail.com>
2021-04-26 10:53:06 +0200

    Enhanced version of a validBST method in exercise 5 - trees

--- Algorithms/Trees/trees/exercises/ex5.java
+++ Algorithms/Trees/trees/exercises/ex5.java
@@ -9,10 +9,18 @@ import trees.exercises.ex2.BST;
  * */
 public class ex5 {
 	
+	/**
+	 * Returns true if a given tree is a valid Binary Search Tree. False otherwise
+	 * 
+	 * @param BST
+	 * @return boolean
+	 * */
 	public static boolean isBST(BST tree) {
-		return isBST(tree.root());
+		//return isBST(tree.root());
+		return checkValidBST( tree.root(), tree.left(), tree.right() );
 	}
 	
+	@Deprecated
 	private static boolean isBST(BinaryTree<Integer>.Node<Integer> node) {
 		if(node == null)
 			return true;
@@ -28,5 +36,34 @@ public class ex5 {
 		
 		return true;
 	}
+	
+	// ==================== IMPROVED VERSION ====================
+	/**
+	 * Check if a tree is a valid Binary Search Tree given the root, the left node and the right.
+	 * It must check all the nodes below.
+	 * 
+	 * @param BinaryTree<Integer>.Node<Integer> Root
+	 * @param BinaryTree<Integer>.Node<Integer> Left node
+	 * @param BinaryTree<Integer>.Node<Integer> Right node
+	 * @return boolean
+	 * */
+	private static boolean checkValidBST(BinaryTree<Integer>.Node<Integer> root, 
+			BinaryTree<Integer>.Node<Integer> left, 
+			BinaryTree<Integer>.Node<Integer> right) {
+		
+		if(root == null)
+			return true;
+		
+		if(left != null && left.value > root.value)
+			return false;
+		
+		if(right != null && right.value < root.value) 
+			return false;
+		
+		return checkValidBST(root.left, left, root) && 
+				checkValidBST(root.right, root, right);
+		
+	}
+	
 
 }
