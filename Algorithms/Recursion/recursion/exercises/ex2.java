8a22b7619954716d662a19e5857f4aabe357fa59
Luisa13 <luisatrece@gmail.com>
2021-08-15 15:57:22 +0200

    minor error

---
+++ Algorithms/Recursion/recursion/exercises/ex2.java
@@ -0,0 +1,15 @@
+package recursion.exercises;
+
+/**
+ * Robot in a Grid: Imagine a robot sitting on the upper left corner of a grid with r rows and c columns. 
+ * The robots can only move in two directions, right and down, but certain cells are 'off limits' such that
+ * the robot cannot step on them. Design an algorithm to find the path for the robot from the top left to 
+ * the bottom right.
+ * 
+ * @author luisa
+ * */
+public class ex2 {
+	public void robotGrid(int grid[][], int c, int r) {
+		
+	}
+}
