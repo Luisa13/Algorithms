4d2f3d871a53e00c55f9bca58c685eb02fba67c3
Luisa13 <luisatrece@gmail.com>
2021-09-23 18:35:40 +0200

    Adds backtracking problem of powerset

---
+++ Algorithms/Recursion/recursion/exercises/ex4.java
@@ -0,0 +1,32 @@
+package recursion.exercises;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Method that returns all subset of a set
+ * 
+ * @author luisa
+ * */
+public class ex4 {
+
+	
+	public List<List<Integer>> powerSet(int[] set) {
+		List<List<Integer>>combinations = new ArrayList<List<Integer>>();
+		powerSet(set, combinations, new ArrayList<Integer>(), 0);
+		
+		return combinations;
+	}
+	
+	private void powerSet(int[] set, List<List<Integer>>combinations, List<Integer>options, int pos) {
+		combinations.add(new ArrayList<>(options));
+		
+		for(int i= pos; i<set.length; i++) {
+			options.add(set[i]);
+			powerSet(set, combinations, options, i+1);
+			options.remove(options.size() -1);
+		}
+	}
+	
+	
+}
