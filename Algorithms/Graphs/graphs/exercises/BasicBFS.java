b693b8967f699f539177d18cd97239d46d5806e5
Luisa13 <luisatrece@gmail.com>
2021-10-26 20:02:36 +0200

    Graph problems folder

---
+++ Algorithms/Graphs/graphs/exercises/BasicBFS.java
@@ -0,0 +1,21 @@
+package graphs.exercises;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Traverse a graph in BFS way.
+ * 
+ * @author luisa
+ * */
+public class BasicBFS {
+
+	public static class Graph<T>{
+		private int n_vertex;
+		private List<T> adj = new ArrayList<>();
+		
+		public Graph() {
+			
+		}
+	}
+}
