52a9613ccab8d17e86142e7dbe8a6e8ea4051f5e
Luisa13 <luisatrece@gmail.com>
2021-11-18 17:48:22 +0100

    Adds tests suit to graphs

---
+++ Algorithms/Graphs/graphs/tests/DependencyGraph1_Test.java
@@ -0,0 +1,35 @@
+package graphs.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DependencyGraph1_Test {
+
+	@Test
+	void AcycledGraph_True_Test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void CycledGraph_False_Test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void CycledGraphWithPossibleProjects_False_Test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void TwoDependenciesForOneCourse_True_Test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void TwoCoursesWithOneDependency_True_Test() {
+		fail("Not yet implemented");
+	}
+
+
+}
