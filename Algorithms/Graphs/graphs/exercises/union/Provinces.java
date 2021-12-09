ea20635c4e352b01b074e6494698c6f1eb5063a2
Luisa13 <luisatrece@gmail.com>
2021-12-09 10:22:31 +0100

    Adds provinces to graph exercises

--- Algorithms/Graphs/graphs/exercises/union/Provinces.java
+++ Algorithms/Graphs/graphs/exercises/union/Provinces.java
@@ -1,9 +1,76 @@
 package graphs.exercises.union;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+
+/**
+ * There are n cities. Some of them are connected, while some are not. If city a
+ * is connected directly with city b, and city b is connected directly with city
+ * c, then city a is connected indirectly with city c.
+ * 
+ * A province is a group of directly or indirectly connected cities and no other
+ * cities outside of the group.
+ * 
+ * You are given an n x n matrix isConnected where isConnected[i][j] = 1 if the
+ * ith city and the jth city are directly connected, and isConnected[i][j] = 0
+ * otherwise.
+ * 
+ * Return the total number of provinces.
+ * 
+ * @author luisa
+ * 
+ */
 public class Provinces {
 
+	public static int findCircleNum(int[][] isConnected) {
+		Stack<Integer> stack = new Stack<Integer>();
+		Map<Integer, List<Integer>> map = new HashMap<>();
+		int[] visited = new int[isConnected.length];
+		int index = 0;
+
+		// Build a graph
+		for (int adj[] : isConnected) {
+			List<Integer> adjList = new ArrayList<>();
+			for (int i = 0; i < adj.length; i++) {
+				if (adj[i] > 0)
+					adjList.add(i);
+			}
+			map.put(index, adjList);
+			visited[index] = adjList.size();
+			index++;
+		}
+
+		int count = 0;
+		// Visit all the groups
+		for (int nexNode = 0; nexNode < visited.length; nexNode++) {
+			if (visited[nexNode] > 0) {
+				stack.push(nexNode);
+
+				// Traverse an graph
+				while (!stack.isEmpty()) {
+					int current = stack.pop();
+					if (visited[current] > 0) {
+						visited[current]--;
+
+						for (int child : map.get(current))
+							stack.push(child);
+
+					}
+				}
+				count++;
+			}
+		}
+
+		return count;
+
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int [][] arr = {{1,1,0},{1,1,0},{0,0,1}};
+		int nGroups = findCircleNum(arr);
 
 	}
 
