d2e7828b2c1f448c2b546180aa466c4dbe4ca889
Luisa13 <luisatrece@gmail.com>
2021-12-16 12:02:34 +0100

    basic structure for DFS problems

---
+++ Algorithms/Graphs/graphs/exercises/DFS/SurroundedRegions.java
@@ -0,0 +1,15 @@
+package graphs.exercises.DFS;
+
+/**
+ * Given an m x n matrix board containing 'X' and 'O', capture all regions that
+ * are 4-directionally surrounded by 'X'. A region is captured by flipping all
+ * 'O's into 'X's in that surrounded region.
+ * 
+ * @author luisa
+ */
+public class SurroundedRegions {
+
+	public static void main(String[] args) {
+
+	}
+}
